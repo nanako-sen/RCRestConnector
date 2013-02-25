352c75f898458a9a5e823225cb81fd7d14cb3ab0
nanako-sen <nanako@gmx.net>
2013-02-25 16:14:41 +1300

    Initial Commit

---
+++ RestConnector/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  RestConnector
+//
+//  Created by Anna Walser on 25/02/13.
+//  Copyright (c) 2013 Nanako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RestConnector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestConnector
+//
+//  Created by Anna Walser on 25/02/13.
+//  Copyright (c) 2013 Nanako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
